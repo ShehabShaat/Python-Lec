d07aa6655b02e5d703478e1758bd3c510819e553
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-17 15:43:48 +0300

    update

--- tutorial/Boolean.py
+++ tutorial/Boolean.py
@@ -1,3 +1,38 @@
 name = ""
 print(name.isspace())
 print(name.isascii())
+print("="*50)
+print(100>50)
+print(100<50)
+print("="*50)
+# true value
+bool(100>101)
+print(bool(100<101))
+print(bool("100>101"))
+print(bool(True))
+print([1,2,3,4])
+print("="*50)
+# false value
+print(bool(0))
+print(bool(""))
+print(bool(''))
+print(bool(False))
+print(bool({}))
+print(bool(None))
+
+# Boolean Operator 
+print("="*50)
+age = 22
+country = "Palestine"
+rank = 20
+
+print(age > 16)
+print(country =="Palestine")
+# and
+print(age>16 and country=="Palestine" and rank < 0)
+# or
+print(age>16 or country=="Palestine" or rank < 0)
+# not
+print(age>16 or country=="Palestine" or not rank < 0)
+
+

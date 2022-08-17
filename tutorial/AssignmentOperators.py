323f2e05faf2c52f88bc712037842d34bcf6a095
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-17 16:04:07 +0300

    update

---
+++ tutorial/AssignmentOperators.py
@@ -0,0 +1,19 @@
+
+x = 10 
+y = 20 
+z = x + y
+print(z)
+print("="*10+" x + y "+"="*10)
+
+x = x + y
+print(x)
+
+print("="*10+" x += y "+"="*10)
+x += y # x = x + y
+print(x)
+
+print("="*10+" x -= y "+"="*10)
+x -= y # x = x - y
+print(x)
+
+print("="*20)

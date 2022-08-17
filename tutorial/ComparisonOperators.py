6d6810f5ed596281f2eb0ff9b25a36812d657930
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-17 16:37:41 +0300

    update

---
+++ tutorial/ComparisonOperators.py
@@ -0,0 +1,32 @@
+
+
+# Equal + Not Equal
+print(100==100)
+print(100==200)
+print(100==100.00)
+print("#"*50)
+print(100!=100)
+print(100!=200)
+print(100!=100.00)
+
+print("="*50)
+
+# Less Than + Grater Than
+print(100>100)
+print(100>200)
+print(100>100.00)
+print("#"*50)
+print(100<100)
+print(100<200)
+print(100<100.00)
+
+print("="*50)
+
+#  Grater Than Or Equal , Less Than Or Equal
+print(100>=100)
+print(100>=200)
+print(100>=100.00)
+print("#"*50)
+print(100<=100)
+print(100<=200)
+print(100<=100.00)

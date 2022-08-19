1c6942c640eac102ed41764afafe61b1c1fce3da
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-19 14:04:53 +0300

    update

--- tutorial/ControlFlow.py
+++ tutorial/ControlFlow.py
@@ -1,15 +1,21 @@
 
-
+# if , elif , else , nested if and Trainigs
 
 Uname = input("Enter Your Name : ").capitalize().strip()
 country = input("Enter Your Country : ").capitalize().strip()
+isStudent = input("are you student ?").strip().lower()
 cName = "Python"
 cPrice = 100
 cDiscount = 50
 
 if country == "Palestine" or country == "Egypt":
-    print(f"Hello {Uname} Because You Are from {country}")
-    print(f"The Course \" {cName}\" Prise Is: ${cPrice-80}")
+    
+    if isStudent =="yes" : 
+       print(f"Hello {Uname} Because You Are From {country} And Student")
+       print(f"The Course \" {cName}\" Prise Is: ${cPrice-90}")
+    else :
+        print(f"Hello {Uname} Because You Are from {country}")
+        print(f"The Course \" {cName}\" Prise Is: ${cPrice-80}")
     
 elif country == "USA" : 
     print(f"Hello {Uname} Because You Are from {country}")
@@ -18,5 +24,7 @@ elif country == "USA" :
 else : 
     print(f"Hello {Uname} Because You Are from {country}")
     print(f"The Course \" {cName}\" Prise Is: ${cPrice-cDiscount}")
+    print("="*100)
+    
     
     

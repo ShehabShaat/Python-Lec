c7c74468b111f221b3f64cf5d09893bd1d827525
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-19 20:45:07 +0300

    update

---
+++ tutorial/Loop.py
@@ -0,0 +1,13 @@
+
+# While and Else
+a = 0
+while a < 10:
+    print("I love Python")
+    a += 1
+
+else:
+    print("loop is done")  # true become false
+
+# while False:  error because the condition is False
+    # print("Will not print")  error because the condition is False
+

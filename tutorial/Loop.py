df81ad75b60d5a898fb541ce065362f75cb191c4
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-20 11:35:56 +0300

    update

--- tutorial/Loop.py
+++ tutorial/Loop.py
@@ -1,4 +1,3 @@
-
 # While and Else
 a = 0
 while a < 10:
@@ -9,5 +8,12 @@ else:
     print("loop is done")  # true become false
 
 # while False:  error because the condition is False
-    # print("Will not print")  error because the condition is False
+# print("Will not print")  error because the condition is False
+print("=" * 50)
+myList = ["A", "B", "C", "D", "E", "F", "G", "J", "K", "L"]
+b = 0
+while b < len(myList):
+    print(f"#{str(b+1).zfill(2)} {myList[b]}")
+    b += 1
+
 

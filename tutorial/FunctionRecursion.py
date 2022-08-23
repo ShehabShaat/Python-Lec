04c32dae5b419ef15fbfa5a0db471114f2380661
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-08-23 17:57:06 +0300

    update

---
+++ tutorial/FunctionRecursion.py
@@ -0,0 +1,15 @@
+# wwwwoooorrrlllddd => world
+
+def clean_word(word):
+    if len(word) == 1:
+        return word
+
+    if word[0] == word[1]: # wwwwoooorrrlllddd
+        return clean_word(word[1:]) # wwwoooorrrlllddd
+    else:
+        return word[0] + clean_word(word[1:])
+
+
+print(clean_word("sssssshhhhheeeehhhhaaabbb"))
+
+

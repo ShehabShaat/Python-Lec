c0c2247f3d40eaac20376c89969cdf6e1ce31e06
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-13 13:07:21 +0300

    update

---
+++ tutorial/pylint.py
@@ -0,0 +1,10 @@
+""" this file to test pylint library"""
+
+
+def say_hello(name):
+    ''' this function to return your name with message hello '''
+    msg = "hello"
+    print(f"{msg} {name}")
+
+
+say_hello("shehab")

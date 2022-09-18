13b659ac0abb80a82f0337c2d35a7d25bc878d0b
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-18 19:32:20 +0300

    update

--- README.md
+++ README.md
@@ -33,7 +33,9 @@
 ### [- Review Pillow cheatsheet](https://pillow.readthedocs.io/en/stable/handbook/tutorial.html)
 
 ># [Regular Expressions ](https://github.com/ShehabShaat/Python-Lec/blob/master/tutorial/RegularExpressions.py)
-### [- Review Regular Expressions Cheatsheet](https://www.debuggex.com/cheatsheet/regex/python)
+### [- Reg Exp Cheatsheet](https://www.debuggex.com/cheatsheet/regex/python)
 ### [- Test RegEx  ](https://pythex.org/)
+### [- Test regex101  ](https://regex101.com/)
+
 [//]: # (># [Python 3.9 | 🔥]&#40;&#41;)
 > ### Python 3.9 | 🔥 New [review](https://www.youtube.com/watch?v=wGd0fdJ7cnY)
--- tutorial/RegularExpressions.py
+++ tutorial/RegularExpressions.py
@@ -6,4 +6,48 @@
 # [3] Used In [Credit Card Validation, IP Address Validation, Email Validation]
 # [4] Test RegEx "https://pythex.org/"
 # [5] Characters Sheet "https://www.debuggex.com/cheatsheet/regex/python"
-# -----------------------------------------------------------
+# ----------------------------------------
+# -- Regular Expressions => Quantifiers --
+# ----------------------------------------
+# *	0 or more
+# +	1 or more
+# ?	0 or 1
+# {2}	Exactly 2
+# {2, 5}	Between 2 and 5
+# {2,}	2 or more
+# (,5}	Up to 5
+# +972 597747241=> https://regex101.com/r/GHtySL/1
+# -----------------------------------------------------------------------
+# -- Regular Expressions => Characters Classes Training's --
+# -----------------------------------------------------------------------
+# [0-9] : Number From 0 to 9
+# [^0-9] : Anything Except 0-9
+# [A-Z] : Characters Form A to Z
+# [^A-Z] : Anything Except Characters From A  to Z
+# [a-z]
+# [^a-z]
+# [A-z] : All Characters
+# shabbinessSaat
+# 0597747241
+#  https://regex101.com/r/OcwAmK/1
+# ---------------------------------------
+# -- Regular Expressions => Assertions --
+# ---------------------------------------
+# ^	  Start of String
+# $	  End of string
+# -------------------------
+# Match Email
+# [A-z0-9\.]+@[A-z0-9]+\.[A-z]+
+# ^[A-z0-9\.]+@[A-z0-9]+\.(com|net|org|info)$
+
+# ----------------------------------------------------
+# -- Regular Expressions => Logical Or And Escaping --
+# ----------------------------------------------------
+# |	  Or
+# \	  Escape Special Characters
+# ()  Separate Groups
+# -----------------------------
+
+# (\d-|\d\)|\d>) (\w+)
+# (\d{3}) (\d{4}) (\d{3}|\(\d{3}\))
+# ^(https?://)(www\.)?(\w+)\.(net|org|com|info|me)$

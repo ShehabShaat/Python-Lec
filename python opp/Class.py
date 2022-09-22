3b0cf09d9d78529e4e38a8dffaa9842d4f65ec28
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-22 15:12:02 +0300

    update

---
+++ python opp/Class.py
@@ -0,0 +1,37 @@
+# ----------------------------------------------------------
+# -- Object Oriented Programming => Class Syntax and Info --
+# ----------------------------------------------------------
+# [01] Class is The Blueprint(مخطط) Or Constructor Of The Object
+# [02] Class Instantiate Means Create Instance( الاوبجكت الي بتنشأه من الكلاس)of A Class
+# [03] Instance => Object Created From Class And Have Their Methods and Attributes
+# [04] Class Defined With Keyword class
+# [05] Class Name Written With PascalCase [UpperCamelCase] Style
+# [06] Class May Contains Methods and Attributes
+# [07] When Creating Object Python Look For The Built In __init__ Method
+# [08] __init__ Method Called Every Time You Create Object From Class
+# [09] __init__ Method Is Initialize The Data For The Object
+# [10] Any Method With Two Underscore in The Start and End Called "Dunder" or Magic Method
+# [11] self Refer To The Current Instance Created From The Class And Must Be First Param
+# [12] self Can Be Named Anything
+# [13] In Python You Dont Need To Call new() Keyword To Create Object
+# -------------------------------------------------------------------
+# class Member:
+#     def __init__(self):
+#         print("Hello Class Member")
+
+
+# my_dict = {
+#     "name": "shehab",
+#     "age": 22,
+#     "monthly_salary": 5000,
+#     "yearly_salary": ''
+#
+# }
+# one_member_class = Member()
+# two_member_class = Member()
+# three_member_class = Member()
+# print(one_member_class.__class__)
+# Member()
+# Member()
+# Member()
+# print(dir(str))

80f5fcc4c02e0ab428632582f9b8c12c4ebf1677
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-22 13:12:24 +0300

    update

--- tutorial/RegularExpressions.py
+++ tutorial/RegularExpressions.py
@@ -46,9 +46,89 @@
 # |	  Or
 # \	  Escape Special Characters
 # ()  Separate Groups
+# https://regex101.com/r/Z665jF/1
+# https://regex101.com/r/0pJYBA/1
 # -----------------------------
 
 # (\d-|\d\)|\d>) (\w+)
 # (\d{3}) (\d{4}) (\d{3}|\(\d{3}\))
 # ^(https?://)(www\.)?(\w+)\.(net|org|com|info|me)$
+# https://regex101.com/r/RI0h5C/1
 
+
+# ---------------------------------------------------------
+# -- Regular Expressions => Re Module Search And FindAll --
+# ---------------------------------------------------------
+# search()  => Search A String For A Match And Return A First Match Only
+# findall() => Returns A List Of All Matches and Empty List if No Match
+# ---------------------------------------------------------------------
+# Email Pattern => [A-z0-9\.]+@[A-z0-9]+\.(com|net|org|info)
+# ----------------------------------------------------------
+import re
+
+my_email1 = re.search("[A-Z]", "SheHab")
+my_email2 = re.search(r"[A-Z]{2}", "SHaAt")
+
+print(my_email1)
+print(my_email2)
+
+print(my_email1.span())  # span=(0, 1) => position
+print(my_email2.span())  # span=(3, 4) => position
+
+print(my_email1.string)
+print(my_email2.string)
+
+print(my_email1.group())
+print(my_email2.group())
+
+# your_email = input("Enter your Email: ")
+# is_email = re.search(r"[A-z0-9\.]+@[A-z0-9]+\.[A-z]+", your_email)
+# if is_email:
+#     print("valid email")
+#     print(is_email.group())
+# else:
+#     print("invalid email")
+
+
+your_email = input("Enter your Email: ")
+is_email = re.findall(r"[A-z0-9\.]+@[A-z0-9]+\.[A-z]+", your_email)
+my_List = []
+if is_email != []:
+    my_List.append(is_email)
+    print("valid email and add")
+
+else:
+    print("invalid email")
+
+for email in my_List:
+    print(email)
+
+# ----------------------------------------------------
+# -- Regular Expressions => Re Module Split And Sub --
+# ----------------------------------------------------
+# split(Pattern, String, MaxSplit)  => Return A List Of Elements Splitted On Each Match
+# sub(Pattern, Replace, String, ReplaceCount) => Replace Matches With What You Want
+# ---------------------------------------------------------------------
+string = "I Love Python"
+my_search = re.split(r"\s", string)
+print(my_search)
+
+string = "I Love Python"
+my_search = re.split(r"\s", string, 1)
+print(my_search)
+
+string = "I-Love-Python_And_Django"
+my_search = re.split(r"-|_", string)
+print(my_search)
+
+for _ in enumerate(my_search):  # counter
+    print(_)
+
+for counter, _ in enumerate(my_search):  # counter
+    if len(_) == 2 or len(_) == 1:
+        continue
+    else:
+        print(f"{counter}- {_}")
+
+print(re.sub("\s", "-", "I Love Python"))
+print(re.sub("\s", "-", "I Love Python", 1))

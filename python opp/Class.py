ef0252edb471153473d4aaf00037fa7de6e00d45
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-22 16:27:02 +0300

    update

--- python opp/Class.py
+++ python opp/Class.py
@@ -14,24 +14,39 @@
 # [11] self Refer To The Current Instance Created From The Class And Must Be First Param
 # [12] self Can Be Named Anything
 # [13] In Python You Dont Need To Call new() Keyword To Create Object
-# -------------------------------------------------------------------
-# class Member:
-#     def __init__(self):
-#         print("Hello Class Member")
+# ==============================================================================
+# -- Object Oriented Programming => Instance Attributes and Methods --
+# --------------------------------------------------------------------
+# Self: Point To Instance Created From Class
+# Instance Attributes: Instance Attributes Defined Inside The Constructor
+# -----------------------------------------------------------------------
+# Instance Methods: Take Self Parameter Which Point To Instance Created From Class
+# Instance Methods Can Have More Than One Parameter Like Any Function
+# Instance Methods Can Freely Access Attributes And Methods On The Same Object
+# Instance Methods Can Access The Class Itself
+# -----------------------------------------------------------
+class Member:
+    def __init__(self, f_name, m_name, l_name, gender):
+        self.f_name = f_name
+        self.m_name = m_name
+        self.l_name = l_name
+        self.gender = gender
+
+    def full_name(self):
+        return f"{self.f_name} {self.m_name} {self.l_name}"
+
+    def say_hello(self):
+        if self.gender == "male":
+            return f"Hello Mr {self.f_name} "
+        else:
+            return f"Hello Miss {self.f_name} "
+
+    def full_info(self):
+        # return f"{Member.say_hello(self)} and your full name {Member.full_name(self)}"
+        return f"{self.say_hello()} and your full name {self.full_name()}"
 
 
-# my_dict = {
-#     "name": "shehab",
-#     "age": 22,
-#     "monthly_salary": 5000,
-#     "yearly_salary": ''
-#
-# }
-# one_member_class = Member()
-# two_member_class = Member()
-# three_member_class = Member()
-# print(one_member_class.__class__)
-# Member()
-# Member()
-# Member()
-# print(dir(str))
+member_one = Member("shehab", "essam", "shaat", "male")
+print(member_one.full_name())
+print(member_one.say_hello())
+print(member_one.full_info())

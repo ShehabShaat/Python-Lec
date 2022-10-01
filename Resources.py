3ad8f7b6c3941bb7252a6b77825fd9306f9aea5d
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-10-01 18:12:07 +0300

    update

---
+++ Resources.py
@@ -0,0 +1,26 @@
+# -------------------------
+# -- Outro and Resources --
+# -------------------------
+# Documentations => https://docs.python.org/3/
+# --------------------------------------------
+# Useful Websites:
+# - Real Python     => https://realpython.com/
+# - Programiz       => https://www.programiz.com/python-programming
+# - GeeksforGeeks   => https://www.geeksforgeeks.org/python-programming-language/
+# - W3Schools       => https://www.w3schools.com/python/default.asp
+# - LearnPython     => https://www.learnpython.org/
+# - TutorialsPoint  => https://www.tutorialspoint.com/python/index.htm
+# -----------------------------------------------------------------
+# Collection
+# - https://wiki.python.org/moin/BeginnersGuide/Programmers
+# ---------------------------------------------------------
+# Resources
+# - https://awesome-python.com/
+# ---------------------------------------------------------
+# The Next Level ?
+# - GUI With Tkinter & PyQt
+# - Parsing Html With BeautifulSoup
+# - Manage HTTP Requests With Requests Module
+# - Web Development With Django & Flask & Web.py
+# - The Binary Number System
+# ----------------------------------------------------------
